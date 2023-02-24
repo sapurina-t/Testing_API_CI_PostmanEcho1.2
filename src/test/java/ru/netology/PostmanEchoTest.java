1d0746aab940574dc44611df4df8594a1db2b527
taya sapurina <sapurina0t@gmail.com>
2023-02-24 22:22:58 +0500

    tests fail

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -1,18 +1,36 @@
 package ru.netology;
 
-public class PostmanEchoTest {
-    // Given - When - Then
-// Предусловия
-    given()
-  .baseUri("https://postman-echo.com")
-  .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
-.when()
-  .post("/post")
-// Проверки
-.then()
-  .statusCode(200)
-  .body(/* --> ваша проверка здесь <-- */)
-            ;
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnBalance123() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("balance: 123")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("balance: 0"))
+        ;
+    }
+
+    @Test
+    void shouldReturnSomeData2() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some value")
+                .when()
+                .post("/post")
+                .then()
+                .body("data", equalTo("some value"))
+        ;
+    }
 }
+
